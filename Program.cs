fd94190c55fc40f432c5cc81011c27e6573bccc2
codingnewwbie <abyss6715@gmail.com>
2025-04-14 20:37:21 +0900

    C#Day1
    Program1~3 = 실습
    personalProject : 과제 틀 잡기

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿//namespace ConsoleApp1
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // 챕터1-과제1
+//            //Console.Write("이름을 입력해주세요: ");
+//            //string name = Console.ReadLine();
+//            //Console.Write("나이을 입력해주세요: ");
+//            //int age = int.Parse(Console.ReadLine());
+//            //Console.WriteLine($"환영합니다 {age}살 {name}님");
+
+
+//            // 챕터1-과제2
+//            //Console.Write("두 수를 하나의 공백으로 입력해주세요(예시 = 3 10): ");
+//            //string input = Console.ReadLine();
+//            //string[] numbers = input.Split(" ");
+//            //int number1 = int.Parse(numbers[0]);
+//            //int number2 = int.Parse(numbers[1]);
+//            //Console.WriteLine($"더하기 = {number1 + number2}\n" + $"빼기 = {number1 - number2} \n" + $"곱하기 = {number1 * number2} \n" + $"나누기(몫) = {number1 / number2} \n" + $"나누기(나머지) = { number1 % number2}");
+
+
+//            // 챕터1-과제3
+//            //Console.Write("화씨 온도로 변환할 섭씨 온도를 입력해주세요: ");
+//            //int temp = int.Parse(Console.ReadLine());
+//            //Console.WriteLine((temp * 1.8) + 32 + "°F");
+
+
+//            // 챕터1-과제4
+//            //Console.Write("BMI 지수를 계산하기 위해 키를 입력해주세요(cm, 정수형): ");
+//            //int inputHei = int.Parse(Console.ReadLine());
+//            //double afterHei = inputHei * 0.01;
+//            //Console.Write("BMI 지수를 계산하기 위해 몸무게를 입력해주세요(kg, 정수형): ");
+//            //int wei = int.Parse(Console.ReadLine());
+//            //Console.WriteLine(Math.Round(wei / (afterHei * afterHei),1));
+
+//            //챕터1 예시
+//            //Console.Write("Enter two numbers: ");
+//            //string input = Console.ReadLine();    // "10 20"과 같은 문자열을 입력받음
+
+//            //string[] numbers = input.Split(' ');  // 문자열을 공백으로 구분하여 배열로 만듦
+//            //int num1 = int.Parse(numbers[0]);     // 첫 번째 값을 정수로 변환하여 저장
+//            //int num2 = int.Parse(numbers[1]);     // 두 번째 값을 정수로 변환하여 저장
+
+//            //int sum = num1 + num2;                // 두 수를 더하여 결과를 계산
+
+//            //Console.WriteLine("The sum of {0} and {1} is {2}.", num1, num2, sum);
+
+
+//            //string str = "Hello, World!";
+//            //int index = str.IndexOf("World");
+//            //Console.WriteLine(index);
+
+//            //string str1 = "Hello, World!";
+//            //string newStr = str1.Replace("World", "Universe");
+//            //Console.WriteLine(newStr);
+
+//            //string str2 = "123";
+//            //int num = int.Parse(str2);
+
+//            //int num3 = 123;
+//            //string str4 = num3.ToString();
+
+
+//            //string str11 = "Hello";
+//            //string str21 = "World";
+//            //bool isEqual = str11 == str21;
+
+//            //string str12 = "Apple";
+//            //string str22 = "Banana";
+//            //int compare = string.Compare(str12, str22);
+
+//            //string name = "John";
+//            //int age = 30;
+//            //string message = string.Format("My name is {0} and I'm {1} years old.", name, age);
+
+//            //string name2 = "John";
+//            //int age2 = 30;
+//            //string message2 = $"My name is {name2} and I'm {age2} years old.";
+//        }
+//    }
+//}
---
+++ personalProject.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+namespace ConsoleApp1
+    {
+    internal class personalProject
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다. + \n");
+            Console.WriteLine("1.상태보기 \n2.인벤토리 \n3.상점");
+            Console.Write("원하시는 행동을 입력해주세요. \n>>");
+
+            string str = Console.ReadLine();
+
+            Status status = new Status();
+
+            switch (str)
+            {
+                case "1":
+                    MyStatus(status);
+
+                    break;
+                case "2":
+                    Inventory(status);
+                    break;
+                case "3":
+                    Shop(status);
+                    break;
+                default:
+                    Console.WriteLine("잘못된 입력입니다.");
+                    break;
+            }
+
+        }
+
+
+
+        static void MyStatus(Status status)
+            {
+                Console.WriteLine("상태 보기 \n캐릭터의 정보가 표시됩니다.\n");
+                Console.WriteLine($"Lv. {status.level} \nChad ( {status.className} )\n공격력 : {status.attackPoint}\n방어력 : {status.defensePoint}\n체 력 : {status.healthPoint}\nGold : {status.gold} G\n");
+
+                Console.WriteLine("0. 나가기\n");
+
+                Console.Write("원하시는 행동을 입력해주세요. \n>>");
+
+            }
+
+        static void Inventory(Status status)
+          {
+                Console.WriteLine("인벤토리 \n보유 중인 아이템을 관리할 수 있습니다.\n [아이템 목록] \n1. 장착 관리\n0.나가기\n");
+
+                Console.Write("원하시는 행동을 입력해주세요. \n>>");
+           }
+
+        static void Shop(Status status)
+        {
+            Console.WriteLine($"상점 \n필요한 아이템을 얻을 수 있는 상점입니다.\n [보유 골드]\n{status.gold} G \n");
+
+            Console.WriteLine("[아이템 목록]");
+            Console.WriteLine("- 수련자 갑옷 | 방어력 + 5 | 수련에 도움을 주는 갑옷입니다. | 1000 G");
+            Console.WriteLine("- 무쇠갑옷 | 방어력 + 9 | 무쇠로 만들어져 튼튼한 갑옷입니다. | 2000 G");
+            Console.WriteLine("- 스파르타의 갑옷 | 방어력 + 15 | 스파르타의 전사들이 사용했다는 전설의 갑옷입니다.| 3500 G");
+            Console.WriteLine("- 낡은 검 | 공격력 + 2 | 쉽게 볼 수 있는 낡은 검 입니다. | 600 G");
+            Console.WriteLine("- 청동 도끼 | 공격력 + 5 | 어디선가 사용됐던거 같은 도끼입니다. | 1500 G");
+            Console.WriteLine("- 스파르타의 창 | 공격력 + 7 | 스파르타의 전사들이 사용했다는 전설의 창입니다. | 3500 G");
+            Console.WriteLine("1.아이템 구매\n2.나가기");
+            Console.Write("원하시는 행동을 입력해주세요. \n>>");
+        }
+
+
+
+
+        struct Status()
+        {
+            public string level = "01";
+            public string name = "Jone";
+            public string className = "전사";
+            public int attackPoint = 10;
+            public int defensePoint = 5;
+            public int healthPoint = 100;
+            public int gold = 1500;
+        }
+    }
+}
