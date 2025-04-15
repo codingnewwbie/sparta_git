21218be7422fa604b1a559b57642d37cc25e5a53
codingnewwbie <abyss6715@gmail.com>
2025-04-15 21:25:27 +0900

    250415 실습 코드들

---
+++ DelegateTest.cs
@@ -0,0 +1,94 @@
+﻿namespace ConsoleApp1
+{
+    internal class DelegateTest
+    {
+        //delegate void MyDelegate(string message);
+
+        //static void Method1(string message)
+        //{
+        //    Console.WriteLine("Method1: " + message);
+        //}
+
+        //static void Method2(string message)
+        //{
+        //    Console.WriteLine("Method2: " + message);
+        //}
+
+
+        //// 델리게이트 선언
+        //public delegate void EnemyAttackHandler(float damage);
+
+        //// 적 클래스
+        //public class Enemy
+        //{
+        //    // 공격 이벤트
+        //    public event EnemyAttackHandler OnAttack;
+
+        //    // 적의 공격 메서드
+        //    public void Attack(float damage)
+        //    {
+        //        // 이벤트 호출
+        //        OnAttack?.Invoke(damage);
+        //        // null 조건부 연산자
+        //        // null 참조가 아닌 경우에만 멤버에 접근하거나 메서드를 호출
+        //    }
+        //}
+
+        //// 플레이어 클래스
+        //public class Player
+        //{
+        //    // 플레이어가 받은 데미지 처리 메서드
+        //    public void HandleDamage(float damage)
+        //    {
+        //        // 플레이어의 체력 감소 등의 처리 로직
+        //        Console.WriteLine("플레이어가 {0}의 데미지를 입었습니다.", damage);
+        //    }
+        //}
+
+
+        static int Add(int x, int y)
+            { return x + y; }
+
+
+        static void PrintMessage(string message) { Console.WriteLine(message); }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Func<int, int, int> addFunc = Add;
+            int Result = addFunc(3, 5);
+
+            Console.WriteLine(Result);
+
+            Action<string> message = PrintMessage;
+            Console.WriteLine(message);
+
+
+
+            //// 적 객체 생성
+            //Enemy enemy = new Enemy();
+
+            //// 플레이어 객체 생성
+            //Player player = new Player();
+
+            //// 플레이어의 데미지 처리 메서드를 적의 공격 이벤트에 추가
+            //enemy.OnAttack += player.HandleDamage;
+
+            //// 적의 공격
+            //enemy.Attack(10.0f);
+
+
+
+
+            //MyDelegate myDelegate = Method1;
+            //myDelegate += Method2;
+
+            //myDelegate("Hello");
+
+        }
+    }
+}
+
+
---
+++ InterfaceTest.cs
@@ -0,0 +1,131 @@
+﻿//using static ConsoleApp1.InterfaceTest;
+
+//namespace ConsoleApp1
+//{
+//    internal class InterfaceTest
+//    {
+//        //public interface IMovable
+//        //{
+//        //    void Move(int x, int y);
+//        //}
+
+//        //public class Player : IMovable
+//        //{
+//        //    public void Move(int x, int y)
+//        //    {
+
+//        //    }
+
+//        //}
+
+//        //public class Enermy : IMovable
+//        //{
+//        //    public void Move(int x, int y)
+//        //    {
+
+//        //    }
+
+//        //}
+
+//        //    public interface IUsable
+//        //{
+//        //    void Use();
+//        //}
+
+//        //public class Item : IUsable
+//        //{
+
+//        //    public string Name { get; set; }
+//        //    public void Use()
+//        //    {
+//        //        Console.WriteLine($"아이템 {Name}을 사용하셨습니다.");
+//        //    }
+//        //}
+
+//        //public class Player
+//        //{      public void UseItem(IUsable item)
+//        //    {
+//        //        item.Use();
+//        //    }
+//        //}
+
+//        public enum Month
+//        {
+//            Jar = 1,
+//            Feb,
+//            Mar,
+//            Apr,
+//            May,
+//            Jun,
+//            Jul,
+//            Aug,
+//            Sep,
+//            Oct,
+//            Nov,
+//            Dec,
+//        }
+
+//        public static void ProcessMonth(int month)
+//        {
+//            if (month >= (int) Month.Jar && month <= (int)Month.Dec)
+//            {
+//                Month selectMonth = (Month)month;
+//                Console.WriteLine($"선택한 월은 {selectMonth}입니다.");
+//            }
+//        }
+
+
+//        // NegativeNumberException을 실행하면 Exception Exception의 base가 먼저 실행되고, 이후 자신의 message가 실행됨.
+//        public class NegativeNumberException : Exception
+//        {
+//            public NegativeNumberException(string message) : base(message)
+//            {
+//            }
+//        }
+
+
+
+
+
+
+//        static void Main(string[] args)
+//        {
+
+//            //int userInput = 4;
+//            //ProcessMonth(userInput);
+
+
+//            //Player player = new Player();
+//            //Item item = new Item() { Name = "Health potion"};
+
+//            //player.UseItem(item);
+
+//            //IMovable movableObject1 = new Enermy();
+//            //IMovable movableObject2 = new Player();
+
+//            //movableObject1.Move(1, 2);
+//            //movableObject2.Move(1, 9);
+
+//            try
+//            {
+//                int number = -10;
+//                if (number < 0)
+//                {
+//                    throw new NegativeNumberException("음수는 처리할 수 없습니다.");
+//                }
+//            }
+//            catch (NegativeNumberException ex)
+//            {
+//                Console.WriteLine(ex.Message);
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine("예외가 발생했습니다: " + ex.Message);
+//            }
+
+
+
+
+//        }
+//    }
+//}
